041599c88f8b5409337cadde838927a91d5c5db5
Aliburus <aliburus1905@gmail.com>
2025-07-16 16:06:23 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,14 +106,12 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Resizer Pro" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
 
-        {/* Google AdSense */}
-        {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID && (
-          <script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
-            crossOrigin="anonymous"
-          />
-        )}
+        {/* Google AdSense Doğrulama */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2671030595283126"
+          crossOrigin="anonymous"
+        />
 
         {/* Hreflang tags for multilingual SEO */}
         <link
